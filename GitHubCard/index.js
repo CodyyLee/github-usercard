3ebc4cf2bc7f4ba99d7cb82af09258ff23840006
Cody Garrett <cody.l.garrett22@gmail.com>
2019-10-03 17:04:00 -0600

    project complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const entry = document.querySelector(".cards");
+axios.get('https://api.github.com/users/CodyyLee').then(function(response) {
 
+  entry.appendChild(cardFunction(response.data.avatar_url, response.data.name, response.data.login, response.data.location, response.data.html_url, response.data.followers, response.data.following, response.data.bio));
+}).catch(function(error){
+  console.log(error);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -14,6 +20,7 @@
            create a new component and add it to the DOM as a child of .cards
 */
 
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -24,7 +31,19 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tetondan","dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"];
+
+  followersArray.forEach(function(element) {
+    axios.get(`https://api.github.com/users/${element}`).then(function(response){
+      entry.appendChild(cardFunction(response.data.avatar_url, response.data.name, response.data.login, response.data.location, response.data.html_url, response.data.followers, response.data.following, response.data.bio))
+    }).catch(function(error) {
+      console.log(error);
+    });
+  });
+  
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +64,47 @@ const followersArray = [];
 </div>
 
 */
+
+function cardFunction(userImg, userName, userUsername, userLocation, userAddress, userFollowers, userFollowing, userBio) {
+  const card = document.createElement("div");
+  const img = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profileLink = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  img.src = userImg;
+  name.textContent = userName;
+  username.textContent = userUsername;
+  location.textContent = `Location: ${userLocation}`;
+  profileLink.textContent = `Profile: ${userAddress}`;
+  followers.textContent = `Followers: ${userFollowers}`;
+  following.textContent = `Following: ${userFollowing}`;
+  bio.textContent = userBio;
+
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
